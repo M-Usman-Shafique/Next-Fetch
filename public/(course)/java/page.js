5b317f8027fc61268af2a0d9b8a1e487077fbfd1
M-Usman-Shafique <hafizusmanshafiq@gmail.com>
2024-06-23 21:19:07 +0500

    go

---
+++ public/(course)/java/page.js
@@ -0,0 +1,13 @@
+// Group Routing
+// Writing course folder inside braces hides & skips the folder.
+// Now http://localhost:3000/java will lead to the Java page.
+
+import React from 'react'
+
+function Java() {
+  return (
+    <div>Java</div>
+  )
+}
+
+export default Java
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,14 +1,14 @@
-// import UseRouter from "@/components/UseRouter";
+import UseRouter from "@/components/UseRouter";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    {/* <LearnLink /> */}
+    <LearnLink />
     {/* <UseRouter /> */}
     <h1>Welcome to Next JS</h1>
     <p>
-    <Link href="/fetch/CSR" style={{color: 'white'}}>See Client Side Data Fetching</Link>
+    {/* <Link href="/fetch/CSR" style={{color: 'white'}}>See Client Side Data Fetching</Link> */}
     </p>
     </main>
   );
